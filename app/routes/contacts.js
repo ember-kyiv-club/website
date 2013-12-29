ab9293d205cf95da8b4c389339e3a7892a61c7ea
Opak Alex <opak.alexandr@gmail.com>
2013-12-29 11:56:43 +0200

    close #10

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -4,8 +4,9 @@ export default Ember.Route.extend({
     submit: function(){
       var controller = this.get('controller');
       if (!Ember.isEmpty(controller.get('model.email')) && !Ember.isEmpty(controller.get('model.message'))){
-        //Todo send message to server
         controller.set('model.isSent', true);
+        window.open("mailto:%@?subject=Ember Kyiv Club&body=%@".fmt("ember-kyiv-club@gmail.com",
+          controller.get('model.message')));
       }
     }
   },
