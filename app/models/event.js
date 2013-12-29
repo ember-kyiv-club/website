4541c3d2e597becddfcafb9dc70e632d04e9418c
Opak Alex <opak.alexandr@gmail.com>
2013-12-29 15:36:15 +0200

    add first event

--- app/models/event.js
+++ app/models/event.js
@@ -18,12 +18,13 @@ Event.FIXTURES = [
   {
     id:          1,
     title:       "Ember.js - инструмент быстрого прототипирования",
-    date:        "12 - 13 февраля 2014 года",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    image:       "assets/images/static/event.jpg",
-    schedules:   [1,2,3],
-    lat:         45.375905,
-    lon:         36.033998
+    date:        "15 февраля 2014 года",
+    description: "Первая киевская встреча разработчиков использующих Ember.js. Обсуждение open source проектов с использованием Ember.js," +
+                 "призентация Ember-Admin. Рассмотрение Ember.js в качестве платформы для быстрого протитипирования web решений.",
+    image:       "assets/images/static/event.jpg"
+//    schedules:   [1,2,3],
+//    lat:         45.375905,
+//    lon:         36.033998
   }
 ];
 
