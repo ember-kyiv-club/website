c1c7ba3d285897c885fcc206348653f116451998
lessless <me@lessless.pp.ua>
2013-12-31 12:59:43 +0200

    add evemt long title text test

--- tests/unit/models/event_test.js
+++ tests/unit/models/event_test.js
@@ -19,5 +19,15 @@ test('generate tweet text with short title', function(){
     record = store.createRecord('event', {title: 'this text is only forty two symbols length'});
   });
 
-  equal(record.get('tweet_text'), "Присоединяйтесь ко мне на \"this text is only forty two symbols length\"")
+  equal(record.get('tweet_text'), "Присоединяйтесь ко мне на \"this text is only forty two symbols length\"");
+});
+test('generate tweet text with long title', function(){
+  expect(1);
+  var record;
+
+  Ember.run(function(){
+    record = store.createRecord('event', {title: 'this text is really very, very long and exceeds tweet limit'});
+  });
+
+  equal(record.get('tweet_text'), "Присоединяйтесь ко мне на \"this text is really very, very long and exceeds twe...\"");
 });
