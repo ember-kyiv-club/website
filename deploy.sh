eff0a5897ebedbc5ff9c6ec876d07ce93b83fae8
Opak Alex <opak.alexandr@gmail.com>
2014-10-22 17:28:51 +0300

    modify deploy

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 # Make sure we're in a good state.
 git checkout master
-git pull
+git pull origin master
 rm -rf dist
 ember build --environment production
 
 # Move to the "release" branch and make everything work.
 git checkout gh-pages
-git pull
+git pull origin gh-pages
 ls -1 | grep -v -E '^dist$' | xargs rm -rf
 mv dist/* ./
 mv dist/.* ./
@@ -16,7 +16,7 @@ rmdir dist
 
 # Add everything and push it.
 git commit -am "Update."
-git push
+git push origin gh-pages
 git checkout master
-git pull
+git pull origin master
 ./install.sh
