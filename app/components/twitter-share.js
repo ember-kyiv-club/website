e5192a3ec3c85c4ba99832679543735ce1070d8e
Opak Alex <opak.alexandr@gmail.com>
2014-10-23 20:31:44 +0300

    Update.

--- app/components/twitter-share.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  tagName: 'a',
-  classNames: 'twitter-share-button',
-  attributeBindings: ['data-size', 'data-url', 'data-text', 'data-hashtags']
-});
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf vendor node_modules tmp
-npm install
-bower install
-git checkout -f  vendor
-rm .gitkeep
