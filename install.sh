b0b026a81a571f3b5faf1fd771038a8eb0b50aaf
Opak Alex <opak.alexandr@gmail.com>
2014-10-23 22:32:48 +0300

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
+rm .gitkeep
+rm crossdomain.xml
+rm index.html
+rm robots.txt
 rm -rf vendor node_modules tmp
 npm install
 bower install
 git checkout -f  vendor
-rm .gitkeep
+
